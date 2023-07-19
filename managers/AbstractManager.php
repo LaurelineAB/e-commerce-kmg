da80fec946127e7ed5b89ea55f60f33acbe5d048
Fonzard <francisrouxel2@gmail.com>
2023-07-19 13:22:07 +0000

    changement de BDD

--- managers/AbstractManager.php
+++ managers/AbstractManager.php
@@ -6,9 +6,9 @@ abstract class AbstractManager {
     public function __construct()
     {
         $this->db = new PDO(
-            $connexionString = "mysql:host=db.3wa.io;port=3306;dbname=marierichir_ecommerce",
-            $username = "marierichir",
-            $password = "a616eefc0b8af8e5fb785ae6b42c19f1"
+            $connexionString = "mysql:host=db.3wa.io;port=3306;dbname=francisrouxel_e-commerce",
+            $username = "francisrouxel",
+            $password = "acadbb28886b6985666cd7eff4651f1d"
         );
     }
 }
